4de69c23b46e58b250cc4966ff8f3970b5603c6e
Dmitry <i@aaea.ru>
2012-08-19 23:56:29 +0600

    Template again

---
+++ template/js/json.js
@@ -0,0 +1,107 @@
+[
+    {
+        "url": "http://www.royalhigh.edin.sch.uk/departments/departments/cdt/scd%2520s4%2520folios/eiffel%2520drawing%25202a.jpg", 
+        "rating": 0, 
+        "title": "scd s4 dane&#39;s cd tower page", 
+        "id": 271, 
+        "size": "box-2"
+    }, 
+    {
+        "url": "http://farm8.staticflickr.com/7086/7258623962_297441a0c5_z.jpg", 
+        "rating": 0, 
+        "title": "Max Beckmann - View from the window of the Eiffel Tower, 1930 at ...", 
+        "id": 272, 
+        "size": "box-2"
+    }, 
+    {
+        "url": "http://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/France_Paris-Eiffel-Tower_2005.jpg/800px-France_Paris-Eiffel-Tower_2005.jpg", 
+        "rating": 0, 
+        "title": "The Steampunk Home: La Tour Eiffel", 
+        "id": 273, 
+        "size": "box-2"
+    }, 
+    {
+        "url": "http://static.europeupclose.com/wp-content/uploads/2011/01/FIreworks_from_the_Eiffel-Tower.jpg", 
+        "rating": 0, 
+        "title": "Gustave Eiffel&#39;s Masterpiece: The Eiffel Tower | EuropeUpClose.", 
+        "id": 274, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://www.clemson.edu/caah/history/facultypages/PamMack/lec122sts/tower.jpg", 
+        "rating": 0, 
+        "title": "cowan8", 
+        "id": 275, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://collections.yadvashem.org/photosarchive/s637-469/9657391555532464715.jpg", 
+        "rating": -1, 
+        "title": "Paris, France, 1940, The Eiffel Tower. - A German soldier&#39;s album ...", 
+        "id": 276, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://collections.yadvashem.org/photosarchive/s637-469/8533785797114107781.jpg", 
+        "rating": -1, 
+        "title": "Paris, France, 1940, The Eiffel Tower and a view of the city. - A ...", 
+        "id": 277, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://4.bp.blogspot.com/-xB-dGhaVcr8/T7vnGPGabXI/AAAAAAAADpU/uzMOpAUTdcU/s1600/eiffel-tower.jpg", 
+        "rating": 0, 
+        "title": "Freedom Writing: Open Up That Golden Gate", 
+        "id": 278, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://pcdn.500px.net/8103349/67a635d417db27980185b40ad677f53d49313393/4.jpg", 
+        "rating": 0, 
+        "title": "500px / Photo &quot;Eiffel Tower (Paris) 1940 vs. 2012&quot; by Johan Pages", 
+        "id": 279, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://farm3.static.flickr.com/2427/4090692304_aeab311474.jpg", 
+        "rating": 0, 
+        "title": "Flickriver: Most interesting photos from Original 1940s Photos pool", 
+        "id": 280, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://pcdn.500px.net/8103349/67a635d417db27980185b40ad677f53d49313393/3.jpg", 
+        "rating": 0, 
+        "title": "500px / Photo &quot;Eiffel Tower (Paris) 1940 vs. 2012&quot; by Johan Pages", 
+        "id": 281, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://farm2.staticflickr.com/1194/539626886_491f3a000c.jpg", 
+        "rating": 0, 
+        "title": "1940&#39;s Paris model on Eiffel Tower | Flickr - Photo Sharing!", 
+        "id": 282, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://timesonline.typepad.com/photos/uncategorized/2008/05/01/eiffel_tower.jpg", 
+        "rating": 0, 
+        "title": "The City of Lights - Literature Network Forums", 
+        "id": 283, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://hqwallpapers.eu/wallpaper/eiffel_tower_desktop_1940x1213_wallpaper-1066751.jpg", 
+        "rating": -1, 
+        "title": "eiffel tower HD Wallpaper - General (#", 
+        "id": 284, 
+        "size": "box-1"
+    }, 
+    {
+        "url": "http://www.redcross.org/museum/exhibits/ww1/01_Hine_eiffel_tower.jpg", 
+        "rating": 0, 
+        "title": "American Red Cross Museum", 
+        "id": 285, 
+        "size": "box-1"
+    }
+]
