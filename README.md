cd0395c55d0d25379e57bc1da8630125acb635e1
Chodex <xchode@gmail.com>
2012-08-19 16:23:23 +0400

    Template add

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ http://miracleslive.com/
 
 About
 -----
-MiraclesLive is a visual composition existence miracles of the world based on tag search in instagram, flickr and google images.
+MiraclesLive is a visual composition existing miracles of the world based on tag search in instagram, flickr and google images.
 
 
 Code for Djangodash // From Ufa, Russia
---
+++ template/js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+
+
+
+	$('#imagesLoad').masonry({
+		itemSelector: '.box',
+		isAnimated: true,
+		isFitWidth: true,
+	});
+	
+	$('#imagesLoad .img').hover(
+	function(){
+		$(this).html('<div class="eye"></div>')
+	},
+	function(){
+		$(this).empty()
+	})
+	
+	
+	
+	
+	$('#imagesLoad .img').click(function(){
+		var img = $(this).css('background-image').split('url(').join('').split(')').join('').split('"').join('')
+
+		$('#lightbox img').attr('src',img)
+		var images_height = $('#lightbox img').attr('src',img)
+		
+		
+		$('.blackBackground').fadeIn()
+		$('#lightbox').css('margin-top',-$('#lightbox img').height()/2)
+		
+	})
+	
+	$('.blackBackground').click(function(){
+		$(this).fadeOut();
+	})
+	
+	
+
+});
