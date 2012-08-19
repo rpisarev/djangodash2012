84b870865883a73e9d634067a6e8e00a17c0e7c1
franz-ru <franz.ru@gmail.com>
2012-08-19 13:48:47 +0600

    views and votes count

--- djangodash2012/apps/core/utils.py
+++ djangodash2012/apps/core/utils.py
@@ -2,5 +2,6 @@ import datetime
 
 def set_cookie(response, key, value, days_expire):
     max_age = days_expire * 24 * 60 * 60
-    expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
-    response.set_cookie(key, value, max_age=max_age, expires=expires, domain=settings.SESSION_COOKIE_DOMAIN, secure=settings.SESSION_COOKIE_SECURE or None)
+    expires = datetime.datetime.strftime(datetime.datetime.utcnow() \
+    + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+    response.set_cookie(key=str(key), value=value,expires=expires, max_age=max_age)
--- djangodash2012/apps/core/views.py
+++ djangodash2012/apps/core/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404, redirect,HttpResponse, RequestContext,HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect,HttpResponse, render_to_response
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Sum
+from django.db.models import Sum, F
 import datetime
 
 from core.models import Image,Vote,Miracle
-from local.utils import get_client_ip, instagram_get_by_tag, google_get
+from local.utils import get_client_ip
 from .utils import set_cookie
 
 
@@ -16,6 +16,13 @@ def main(request, template='main.html'):
 def miracle(request, miracle_slug, template='miracle.html'):
     miracle = get_object_or_404(Miracle, slug = miracle_slug)
     context = ({'miracle':miracle})
+    response = render_to_response(template, context=context)
+    cookie_key = "miracle_%s" % miracle_slug
+    days_expire = 1
+    if not request.COOKIES.get(cookie_key):
+        set_cookie(response,key,value,days_expire)
+        Miracle.objects.filter(slug=miracle_slug).update(views_count=F('views')+1)
+
     return render(request, template, context)
 
 def miracle_year(request, miracle_slug, year):
@@ -24,11 +31,12 @@ def miracle_year(request, miracle_slug, year):
 def vote(request,image_id,value):
     cookie_key = "image_%s"%image_id
     days_expire = 1
-    if request.is_ajax() and not request.COOKIE.get(cookie_key):
+    if request.is_ajax() and not request.COOKIES.get(cookie_key):
         try:
             user_ip = get_client_ip(request)
-            month_ago = datetime.datetime.now()-datetime.timedelta(days=days_expire)
-            Vote.objects.get(user_ip=user_ip,created__gt=month_ago)
+            time_ago = datetime.datetime.now()-datetime.timedelta(days=days_expire)
+            Vote.objects.get(user_ip=user_ip,created__gt=time_ago,image_id=image_id)
+
         except :
                 response = HttpResponse()
                 try:
@@ -38,7 +46,7 @@ def vote(request,image_id,value):
                 vote = Vote()
                 vote.value = 1 if value=='up' else -1
                 vote.user_ip = user_ip
-                vote.image=image
+                vote.image = image
                 vote.created = datetime.datetime.now()
                 vote.save()
 
