cc8856d19168f445c1996071fd3c2c5c9f30550b
franz-ru <franz.ru@gmail.com>
2012-08-19 16:17:44 +0600

    settings prod

--- djangodash2012/settings.py
+++ djangodash2012/settings.py
@@ -14,16 +14,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+if platform.node() != PRODUCTION_HOST:
+    from settings_dev import *
+else:
+    from settings_prod import *
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -164,8 +158,7 @@ LOGGING = {
 
 PRODUCTION_HOST = 'dash.chodex.ru'
 
-if platform.node() != PRODUCTION_HOST:
-    from settings_dev import *
+
 
 INITIAL_YEAR = 1930
 FINAL_YEAR = 2001
